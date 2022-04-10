d8eda57bd9777d3cb9de4e05dd70e7cc725579d7
KmLig <phamxuankhiem1102@gmail.com>
2022-04-10 15:08:21 +0700

    commit on main

---
+++ Lab2/Lab2_1/confusion/src/App.js
@@ -0,0 +1,16 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>        
+    </div>
+  );
+}
+
+export default App;
