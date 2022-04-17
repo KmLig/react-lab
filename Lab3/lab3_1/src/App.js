80ebab45f9fbe785bd42b0bfa9ff4d33b6964f65
KmLig <phamxuankhiem1102@gmail.com>
2022-04-18 03:02:42 +0700

    presentational and container component

--- Lab3/lab3_1/src/App.js
+++ Lab3/lab3_1/src/App.js
@@ -1,26 +1,14 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
 import { Component } from 'react';
+import Main from './components/MainComponent';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES
-    }
-  }
+class App extends Component {  
+    
   render() {
+    
     return (
-      <div>
-        <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Menu</NavbarBrand>
-            </div>
-          </Navbar>
-          <Menu dishes={this.state.dishes}/>
+      <div className="App">
+        <Main/>        
       </div>
     );
   }  
