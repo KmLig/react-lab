f86ee7c6c61d6eabb86eb753fcbc70ad0f2b4ee9
KmLig <phamxuankhiem1102@gmail.com>
2022-04-20 04:10:06 +0700

    Router

---
+++ Lab5/lab5_2/src/components/MainComponent.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Home from './HomeComponent';
+import Menu from './MenuComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { DISHES } from '../shared/dishes';
+import DishDetail  from './DishdetailComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES,
+    };    
+  }
+
+  
+  render() {    
+
+    const HomePage = () => {
+      return(
+          <Home />
+      );
+    }
+    return (
+      <div>
+        <Header />
+        <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Redirect to="/home" />
+          </Switch>      
+        <Footer />
+      </div>
+    );
+  }  
+}
+
+export default Main;
