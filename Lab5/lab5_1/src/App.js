93f0ad7452fc986df8d264085a693e21180c6b1d
KmLig <phamxuankhiem1102@gmail.com>
2022-04-20 02:16:35 +0700

    add Lab5 Header and Footer

---
+++ Lab5/lab5_1/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+
+class App extends Component {  
+    
+  render() {
+    
+    return (
+      <div className="App">
+        <Main/>        
+      </div>
+    );
+  }  
+}
+
+export default App;
